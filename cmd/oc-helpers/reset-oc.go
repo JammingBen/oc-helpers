a1ab48bb8f4cdd0186211c855eb5bde8b7761b79
Jannik Stehle <jannik.stehle@gmail.com>
2021-01-29 10:41:31 +0100

    Roll back oc data reset command because of complications while preservig the config

--- cmd/oc-helpers/reset-oc.go
+++ cmd/oc-helpers/reset-oc.go
@@ -5,8 +5,7 @@ import (
 	"github.com/spf13/cobra"
 	"log"
 	"oc-helpers/pkg/env"
-	oc_os "oc-helpers/pkg/os"
-	"os"
+	"oc-helpers/pkg/os"
 )
 
 func init() {
@@ -15,32 +14,14 @@ func init() {
 
 var resetCmd = &cobra.Command{
 	Use:   "reset-oc",
-	Short: "Reset all ownCloud data while preserving the config",
+	Short: "Reset your ownCloud installation (all data will be lost)",
 	Run: func(cmd *cobra.Command, args []string) {
 		ocPath := env.Get("OC_PATH")
-		configFilePath := fmt.Sprintf("%s/oc-config.json", oc_os.GetBasePath())
-		configFile, _ := os.Create(configFilePath)
-		defer func() {
-			_ = os.Remove(configFilePath)
-		}()
-
-		err := oc_os.RunCmd("/usr/bin/php occ config:list --private", ocPath, configFile)
+		err := os.RemoveContents(fmt.Sprintf("%s/data", ocPath))
 		if err != nil {
 			log.Panicf(err.Error())
 		}
-		err = oc_os.RemoveContents(fmt.Sprintf("%s/data", ocPath))
-		if err != nil {
-			log.Panicf(err.Error())
-		}
-		err = oc_os.ReplaceFileContent(fmt.Sprintf("%s/config/config.php", ocPath), "'installed' => true,", "'installed' => false,")
-		if err != nil {
-			log.Panicf(err.Error())
-		}
-		err = oc_os.RunCmd("/usr/bin/php occ maintenance:install --admin-pass admin", ocPath, nil)
-		if err != nil {
-			log.Panicf(err.Error())
-		}
-		err = oc_os.RunCmd(fmt.Sprintf("/usr/bin/php occ config:import %s", configFilePath), ocPath, nil)
+		err = os.ReplaceFileContent(fmt.Sprintf("%s/config/config.php", ocPath), "'installed' => true,", "'installed' => false,")
 		if err != nil {
 			log.Panicf(err.Error())
 		}
